4c0b60a7e4587f38311963b0a3e8b74216bf1f65
atan333 <amytan@terpmail.umd.edu>
2024-06-18 21:40:18 -0400

    Implementing file upload

--- src/main/java/info/amytan/smartfile/SmartFile.java
+++ src/main/java/info/amytan/smartfile/SmartFile.java
@@ -3,25 +3,31 @@ package info.amytan.smartfile;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SmartFile {
 
     public static void main(String[] args) {
-        String path = "/Users/amytan/Downloads/PracticeProblemsExam1.pdf";
-        try {
-            PdfReader reader = new PdfReader(path);
-            PdfDocument pdfDoc = new PdfDocument(reader);
-            for (int i = 1; i <= pdfDoc.getNumberOfPages(); i++) {
-                String text = PdfTextExtractor.getTextFromPage(pdfDoc.getPage(i));
-                System.out.println("Page " + i + ": ");
-                System.out.println(text);
-                System.out.println();
-            }
-            pdfDoc.close();
-        }
+        SpringApplication.run(SmartFile.class, args);
 
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Testing iText
+//        String path = "/Users/amytan/Downloads/PracticeProblemsExam1.pdf";
+//        try {
+//            PdfReader reader = new PdfReader(path);
+//            PdfDocument pdfDoc = new PdfDocument(reader);
+//            for (int i = 1; i <= pdfDoc.getNumberOfPages(); i++) {
+//                String text = PdfTextExtractor.getTextFromPage(pdfDoc.getPage(i));
+//                System.out.println("Page " + i + ": ");
+//                System.out.println(text);
+//                System.out.println();
+//            }
+//            pdfDoc.close();
+//        }
+//
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 }
---
+++ src/main/java/info/amytan/smartfile/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package info.amytan.smartfile;
+
+public class StorageFileNotFoundException extends StorageException {
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
