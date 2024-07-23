b632a71a09e5ac27e949885a4a2ead655564fe35
atan333 <amytan@terpmail.umd.edu>
2024-07-23 14:42:29 -0400

    Handling generic exceptions, displaying message when file size is exceeded

--- src/main/java/info/amytan/smartfile/GlobalExceptionHandler.java
+++ src/main/java/info/amytan/smartfile/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.ModelAndView;
 
 @Component
 @ControllerAdvice
@@ -18,28 +19,15 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     @ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
-    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException ex) {
-        return new ResponseEntity<>("File size exceeds the maximum limit!", HttpStatus.PAYLOAD_TOO_LARGE);
+    public ModelAndView handleMaxSizeException(MaxUploadSizeExceededException ex) {
+        ModelAndView modelAndView = new ModelAndView("uploadForm");
+        modelAndView.addObject("message", "File too large");
+        return modelAndView;
     }
 
-//    public String handleMaxSizeException(MaxUploadSizeExceededException exception) {
-//        Object status = exception.getStatusCode();
-//        System.out.println("ControllerAdvice is being called");
-//
-//        if (status != null) {
-//            int statusCode = Integer.parseInt(status.toString());
-//            System.out.println(statusCode + " is the status code");
-//
-//
-//            if (statusCode == HttpStatus.PAYLOAD_TOO_LARGE.value()) {
-//                return "error-413";
-//            }
-//
-//            else if (statusCode == HttpStatus.NOT_FOUND.value()) {
-//                return "error-404";
-//            }
-//        }
-//
-//        return "error";
-//    }
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleGenericException(Exception ex) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        return modelAndView;
+    }
 }
